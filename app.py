0db13dddc50475603c9b40b60baa3b8df6d0bb1c
BATPATH <brad@batpathtraining.com>
2025-03-02 18:03:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ import matplotlib.pyplot as plt
 # -------------------------
 # Authenticate Google Sheets
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("batpath-credentials.json", scope)
+creds_dict = st.secrets["gcp_service_account"]
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 
 # Open Google Sheet
